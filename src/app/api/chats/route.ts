2d41bd021d0b42cb984b076622fe11250187d024
Lauro Guedes <leowgweb@gmail.com>
2023-04-22 17:51:28 -0300

    install and config prisma

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,6 +1,23 @@
+import { prisma } from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  return NextResponse.json({ body });
+  const chatCreated = await prisma.chat.create({
+    data: {
+      message: body.message,
+    },
+  });
+
+  return NextResponse.json(chatCreated);
+}
+
+export async function GET(request: NextRequest) {
+  const chats = await prisma.chat.findMany({
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+
+  return NextResponse.json(chats);
 }
